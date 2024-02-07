0d52b3e6707ab4e6c2339216e7faaeac3813f660
Alexis <alexis.cabo.87@gmail.com>
2024-02-07 09:35:08 -0300

    inicio ejercicio 2

--- ejercicio_01/ejercicio_01.c
+++ ejercicio_01/ejercicio_01.c
@@ -7,12 +7,6 @@ int main(){
     printf("ingrese dos numeros: ");
     scanf("%d %d",&a,&b);
 
-
-    // printf("Ingrese un numero: ");
-    // scanf("%d",&a);
-    // printf("Ingrese otro numero: ");
-    // scanf("%d",&b);
-
     printf("%d mas %d = %d\n",a,b,a+b);
     printf("%d menos %d = %d\n",a,b,a-b);
     printf("%d por %d = %d\n",a,b,a*b);
